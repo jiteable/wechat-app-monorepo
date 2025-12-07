1cf400542b8ef0f97b2987c1ea9934aa1dc959f5
刘----仨 <15879806727@163.com>
2025-12-07 20:25:52 +0800

    feat(chat): 显示空聊天提示图标和文本 当没有选择具体聊天对象时，在聊天内容区域展示一个友好的空状态提示， 包括一个消息图标和“请选择一个聊天”文字，提升用户体验。

--- easychat-front/src/renderer/src/views/chatContant/ChatContant.vue
+++ easychat-front/src/renderer/src/views/chatContant/ChatContant.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="chat-contant-container">
     <WindowControls />
-    <div class="chat-contant">{{ route.params.id ? route.params.id : 1 }}</div>
+    <div class="chat-contant">
+      <div v-if="route.params.id" class="chat-id">{{ route.params.id }}</div>
+      <div v-else class="empty-chat">
+        <el-icon :size="100" color="#c0c4cc">
+          <Message />
+        </el-icon>
+        <p>请选择一个聊天</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import WindowControls from '@/components/WindowControls.vue'
 import { useRoute } from 'vue-router'
+import { Message } from '@element-plus/icons-vue'
 
 const route = useRoute()
 console.log(route.params.id) // 当前用户ID
@@ -19,5 +28,25 @@ console.log(route.params.id) // 当前用户ID
   display: flex;
   flex-direction: column;
   background-color: rgb(236, 237, 237);
+}
+
+.chat-contant {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.empty-chat {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: #909399;
+}
+
+.empty-chat p {
+  margin-top: 20px;
+  font-size: 16px;
 }
 </style>
